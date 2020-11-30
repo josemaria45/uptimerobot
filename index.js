9ed5f8683afc8f5e74652aec796f9704ec7683f6
josemaria45 <75233593+josemaria45@users.noreply.github.com>
2020-11-30 10:38:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const axios = require("discord.js");
-const urls = "https://glitch.com/edit/#!/gloria-victis"
+const axios = require("axios");
+const urls = "https://gloria-victis.glitch.me"
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
